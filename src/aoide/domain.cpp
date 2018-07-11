27b6a3971256efcbdd42100f182ffd22ed344c5c
Uwe Klotz <uklotz@mixxx.org>
2018-07-11 10:51:41 +0200

    Use kebab-case for serializing aoide enums to JSON

--- src/aoide/domain.cpp
+++ src/aoide/domain.cpp
@@ -490,7 +490,7 @@ void AoideRating::setOwner(QString owner) {
 double AoideLoudness::ebuR128Lufs(double defaultDb) const {
     for (const auto jsonValue : m_jsonArray) {
         DEBUG_ASSERT(jsonValue.isObject());
-        const auto innerValue = jsonValue.toObject().value("ebuR128Lufs");
+        const auto innerValue = jsonValue.toObject().value("ebu-r128-lufs");
         if (innerValue.isDouble()) {
             return innerValue.toDouble(defaultDb);
         }
@@ -501,13 +501,13 @@ double AoideLoudness::ebuR128Lufs(double defaultDb) const {
 void AoideLoudness::setEbuR128Lufs(double ebuR128Lufs) {
     for (auto i = m_jsonArray.begin(); i != m_jsonArray.end(); ++i) {
         DEBUG_ASSERT(i->isObject());
-        auto innerValue = i->toObject().value("ebuR128Lufs");
+        auto innerValue = i->toObject().value("ebu-r128-lufs");
         if (innerValue.isDouble()) {
-            *i = QJsonObject{{"ebuR128Lufs", ebuR128Lufs}};
+            *i = QJsonObject{{"ebu-r128-lufs", ebuR128Lufs}};
             return;
         }
     }
-    m_jsonArray += QJsonObject{{"ebuR128Lufs", ebuR128Lufs}};
+    m_jsonArray += QJsonObject{{"ebu-r128-lufs", ebuR128Lufs}};
 }
 
 /*static*/ const QString AoideScoredSongFeature::kAcousticness = "acousticness";
