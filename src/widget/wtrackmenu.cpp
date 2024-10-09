f351c492a10aa23b69e31cf279522fe44e3d65b7
Uwe Klotz <uwe.klotz@gmail.com>
2024-10-10 00:45:36 +0200

    Add aoide integration

--- src/widget/wtrackmenu.cpp
+++ src/widget/wtrackmenu.cpp
@@ -1278,6 +1278,9 @@ TrackPointer WTrackMenu::getFirstTrackPointer() const {
                 return pTrack;
             }
             // Skip unavailable tracks
+            qWarning() << "WTrackMenu::getFirstTrackPointer: "
+                          "Skipping unavailable track at index"
+                       << index;
         }
         return TrackPointer();
     }
